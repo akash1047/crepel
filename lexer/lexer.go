c5fab562f88e07b80ae701aab2153a67506fed9a
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2023-12-26 23:47:21 +0530

    lexing and lexeing errors

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,66 @@
 package lexer
+
+import (
+	"fmt"
+
+	"github.com/akash1047/crepel/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+
+	errors []string
+}
+
+func (l *Lexer) LastError() string {
+	return l.errors[len(l.errors)-1]
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
+func (l *Lexer) NextToken() (token.Token, bool) {
+	ok := true
+	var tok token.Token
+
+	switch l.ch {
+	case 0:
+		return token.Token{Type: token.EOF, Literal: ""}, true
+	default:
+		tok.Type = token.ILLEGAL
+		tok.Literal = string(l.ch)
+
+		msg := fmt.Sprintf("stray '%c' in program", l.ch)
+		l.errors = append(l.errors, msg)
+		ok = false
+	}
+
+	l.readChar()
+
+	return tok, ok
+}
--- repel/repel.go
+++ repel/repel.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/akash1047/crepel/lexer"
+	"github.com/akash1047/crepel/token"
 )
 
 func Start(in, out *os.File) {
@@ -18,6 +21,13 @@ func Start(in, out *os.File) {
 
 		input := scanner.Text()
 
-		fmt.Fprintln(out, input)
+		l := lexer.New(input)
+
+		for tok, ok := l.NextToken(); tok.Type != token.EOF; tok, ok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+			if !ok {
+				fmt.Fprintf(out, "\x1b[31merror:\x1b[0m %s\n", l.LastError())
+			}
+		}
 	}
 }
